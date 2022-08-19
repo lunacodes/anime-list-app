78654b9573f9bce4d593aa4c4b6f0085e5deb13e
Luna <lunacodes@gmail.com>
2022-08-19 15:28:47 -0400

    Add Login Card styles

--- client/src/components/LoginCard.js
+++ client/src/components/LoginCard.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import Login from './Login';
 import Register from './Register';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.min.js';
 
 import {
 	Col,

d599e321e11591ff124e45532fd074de06b9cc04
Luna <lunacodes@gmail.com>
2022-08-25 20:08:53 -0400

    Re-integrate original server.js code

--- server/server.js
+++ server/server.js
@@ -1,33 +1,57 @@
 import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
+// Local File System
+import path from 'path';
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+// Authentication
 import bodyParser from 'body-parser';
 import jwt from './_helpers/jwt.js';
 import errorHandler from './_helpers/error-handler.js';
+// Routes
+import novelRouter from './routes/novel.js';
 import UserRouter from './routes/user.js';
-// import * as userController from './users/user.controller.js';
-// const { authenticate, getAll } = userController;
-// import authenticate from './users/user.controller.js';
-// import getAll from './users/user.controller.js';
 
+// Environment Variables
+dotenv.config();
+const __dirname = dirname(fileURLToPath(import.meta.url));
+const PORT = process.env.PORT || 8081;
+
+const whitelist = process.env.WHITELISTED_DOMAINS
+	? process.env.WHITELISTED_DOMAINS.split(',')
+	: [];
+
+const corsOptions = {
+	origin: function (origin, callback) {
+		if (!origin || whitelist.indexOf(origin) !== -1) {
+			callback(null, true);
+		} else {
+			callback(new Error('Not allowed by CORS'));
+		}
+	},
+
+	credentials: true,
+};
+
+// App Setup
 const app = express();
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-app.use(cors());
-app.use(UserRouter);
-
-// use JWT auth to secure the api
+app.use(cors(corsOptions));
 app.use(jwt());
-
-// api routes
-// app.use('/', getAll);
-// app.use('/authenticate', authenticate);
-
-// global error handler
 app.use(errorHandler);
+app.use(UserRouter);
+app.use('/users', UserRouter);
+app.use('/novels', novelRouter);
 
-// start server
-const port = process.env.NODE_ENV === 'production' ? 80 : 4000;
-const server = app.listen(port, function () {
-	console.log('Server listening on port ' + port);
+app.use(express.static(path.resolve(__dirname, '../client/build')));
+app.get('*', (req, res) => {
+	res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
+});
+
+const server = app.listen(PORT, function () {
+	const port = server.address().port;
+
+	console.log('App started at port:', port);
 });

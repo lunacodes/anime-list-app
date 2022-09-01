f2f9fa75a1a4ad8cb85d2679a1760407db38f905
Luna <lunacodes@gmail.com>
2022-08-31 20:40:13 -0400

    Remove unused 'User' link from Navbar

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -16,15 +16,6 @@ const Navbar = ({ currentUser, logOut }) => {
 						</Link>
 					</li>
 
-					{currentUser && (
-						<li className='nav-item'>
-							<Link to={'/user'} className='nav-link'>
-								User
-							</Link>
-						</li>
-					)}
-				</div>
-
 				{currentUser ? (
 					<div className='navbar-nav ml-auto'>
 						<li className='nav-item'>

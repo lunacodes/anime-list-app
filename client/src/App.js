7c07005ac4d67892b4872760eb9dac105353a9f7
Luna <lunacodes@gmail.com>
2022-09-04 15:59:18 -0400

    Fix page render blocked, when novels query returns empty data

--- client/src/App.js
+++ client/src/App.js
@@ -57,7 +57,7 @@ const App = () => {
 
 	return (
 		<>
-			{!isLoading && novelsData.length > 0 && (
+			{!isLoading && (
 				<div>
 					<Navbar currentUser={currentUser} logOut={logOut} />
 
--- client/src/components/Gallery.jsx
+++ client/src/components/Gallery.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable */
 import React from 'react';
 import PropTypes from 'prop-types';
 import stringToSlug from '../services/stringToSlug.js';
@@ -6,28 +7,33 @@ import { Link } from 'react-router-dom';
 const Gallery = ({ novels }) => {
   const novel_data = novels;
 
-  const NovelsDisplay = () =>
-    novel_data.map((item, index) => {
-      const id = `${index}-${item.title}`;
-      const title = item.title;
-      const poster = item.poster;
-      const slug = stringToSlug(title);
+  const NovelsDisplay = () => {
+    if (novel_data.length > 0) {
+      return novel_data.map((item, index) => {
+        const id = `${index}-${item.title}`;
+        const title = item.title;
+        const poster = item.poster;
+        const slug = stringToSlug(title);
 
-      return (
-        <div key={id} className='gallery-item'>
-          <Link to={`/novels/${slug}`}>
-            <img
-              src={poster}
-              alt={title}
-              className='gallery-img'
-              width='266'
-              height='400'
-            />
-            <h3>{title}</h3>
-          </Link>
-        </div>
-      );
-    });
+        return (
+          <div key={id} className='gallery-item'>
+            <Link to={`/novels/${slug}`}>
+              <img
+                src={poster}
+                alt={title}
+                className='gallery-img'
+                width='266'
+                height='400'
+              />
+              <h3>{title}</h3>
+            </Link>
+          </div>
+        );
+      });
+    }
+
+    return <div>There are no novels that match that search</div>;
+  };
 
   return (
     <>

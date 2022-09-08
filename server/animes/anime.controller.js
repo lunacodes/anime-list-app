bd58c4884a048613b7b822d67376dee426fc145c
Luna <lunacodes@gmail.com>
2022-09-08 15:25:24 -0400

    Remove DB related functionality from Anime, since the info is being pulled from Kitsu API, and not stored locally

--- server/animes/anime.controller.js
+++ server/animes/anime.controller.js
@@ -1,58 +1,14 @@
 import express from 'express';
-import {
-	addAnime,
-	deleteAnimeById,
-	fetchAnimes,
-	findAnimeById,
-	listAnimes,
-	updateAnimeById,
-} from '../animes/anime.service.js';
+import { fetchAnimes } from '../animes/anime.service.js';
 
 const AnimeRouter = express.Router();
 AnimeRouter.use(express.json());
 
-// Add a new anime
-AnimeRouter.route('/add').post((req, res) => {
-	const title = req.query.title;
-	const score = req.query.score;
-	const progress = req.query.progress;
-	const tags = req.query.tags;
-
-	addAnime(res, title, score, progress, tags);
-});
-
-// Delete a anime
-AnimeRouter.route('/delete').delete((req, res) => {
-	const id = req.body.id;
-
-	deleteAnimeById(res, id);
-});
-
-// Fetch a anime
+// Fetch Anime
 AnimeRouter.route('/fetch').get((req, res) => {
 	const queryStr = req.query.query;
 
 	fetchAnimes(res, queryStr);
-});
-
-// Find a single anime by ID
-AnimeRouter.route('/:id').get((req, res) => {
-	const id = req.body.id;
-	// console.log(res);
-
-	findAnimeById(res, id);
-});
-
-// List All Animes
-AnimeRouter.route('').get((req, res) => {
-	listAnimes(res);
-});
-
-// Update a anime by ID
-AnimeRouter.route('/update/:id').post((req, res) => {
-	const id = req.body.id;
-
-	updateAnimeById(res, id);
 });
 
 export default AnimeRouter;
--- server/animes/anime.service.js
+++ server/animes/anime.service.js
@@ -1,36 +1,10 @@
-import Anime from '../animes/anime.model.js';
 import fetch from 'cross-fetch';
-
-// Add Anime
-export async function addAnime(res, title, score, progress, tags) {
-	const anime = new Anime({
-		title: title,
-		score: score,
-		progress: progress,
-		tags: tags,
-	});
-	await anime.save();
-
-	console.log('Anime was added:\n', anime);
-	res.json(anime);
-}
-
-// Delete Anime
-export async function deleteAnimeById(res, id) {
-	Anime.findByIdAndDelete(id, (err, anime) => {
-		if (err) {
-			console.log(err);
-		} else {
-			res.json(anime);
-			console.log(`Deleted anime: ${anime}`);
-		}
-	});
-}
 
 // Fetch animes
 export function fetchAnimes(res, queryStr) {
 	const base = '//kitsu.io/api/edge/anime';
-	const url = queryStr.length > 0 ? `${base}?filter[text]=${queryStr}` : base;
+	const url =
+		queryStr && queryStr.length > 0 ? `${base}?filter[text]=${queryStr}` : base;
 
 	fetch(url)
 		.then((res) => {
@@ -46,50 +20,4 @@ export function fetchAnimes(res, queryStr) {
 		.catch((err) => {
 			console.error(err);
 		});
-}
-
-// Find Anime
-export async function findAnimeById(res, id) {
-	Anime.findById(id, (err, result) => {
-		if (err) {
-			console.log(err);
-		} else {
-			console.log(result);
-			res.json(result);
-		}
-	});
-}
-
-// List All Animes
-export async function listAnimes(res) {
-	Anime.find({}, (err, result) => {
-		if (err) {
-			console.log(err);
-		} else {
-			console.log('displayed animes');
-			res.json(result);
-		}
-	});
-}
-
-// Update Anime
-export async function updateAnimeById(res, id) {
-	const newValues = {
-		title: res.query.title,
-		score: res.query.score,
-		progress: res.query.progress,
-		tags: res.query.tags,
-	};
-
-	Anime.findByIdAndUpdate(id, newValues, (err, anime) => {
-		console.log(id);
-		console.log(`new values: \n${newValues}`);
-
-		if (err) {
-			console.log(err);
-		} else {
-			res.json(anime);
-			console.log(`Updated anime: ${anime}`);
-		}
-	});
 }

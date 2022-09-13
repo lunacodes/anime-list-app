89775c153cffb33a0cda684727a0c485d69bd70e
Luna <lunacodes@gmail.com>
2022-09-12 20:59:12 -0400

    Combine Login & Register forms into single tabbed card

--- client/src/components/Register.js
+++ client/src/components/Register.js
@@ -122,99 +122,99 @@ const Register = () => {
 	};
 
 	return (
-		<div className='col-md-12'>
-			<div className='card card-container'>
-				<img
-					src='//ssl.gstatic.com/accounts/ui/avatar_2x.png'
-					alt='profile-img'
-					className='profile-img-card'
-				/>
+		<>
+			<img
+				src='//ssl.gstatic.com/accounts/ui/avatar_2x.png'
+				alt='profile-img'
+				className='profile-img-card'
+			/>
 
-				<Form onSubmit={handleRegister} ref={form}>
-					{!successful && (
-						<div>
-							<div className='form-group'>
-								<label htmlFor='firstName'>First Name</label>
-								<Input
-									type='text'
-									className='form-control'
-									name='firstName'
-									value={firstName}
-									onChange={onChangeFirstName}
-									validations={[required, vname]}
-								/>
-							</div>
-
-							<div className='form-group'>
-								<label htmlFor='lastName'>Last Name</label>
-								<Input
-									type='text'
-									className='form-control'
-									name='lastName'
-									value={lastName}
-									onChange={onChangeLastName}
-									validations={[required, vname]}
-								/>
-							</div>
-
-							<div className='form-group'>
-								<label htmlFor='username'>Username</label>
-								<Input
-									type='text'
-									className='form-control'
-									name='username'
-									value={username}
-									onChange={onChangeUsername}
-									validations={[required, vusername]}
-								/>
-							</div>
-
-							<div className='form-group'>
-								<label htmlFor='email'>Email</label>
-								<Input
-									type='text'
-									className='form-control'
-									name='email'
-									value={email}
-									onChange={onChangeEmail}
-									validations={[required, validEmail]}
-								/>
-							</div>
-
-							<div className='form-group'>
-								<label htmlFor='password'>Password</label>
-								<Input
-									type='password'
-									className='form-control'
-									name='password'
-									value={password}
-									onChange={onChangePassword}
-									validations={[required, vpassword]}
-								/>
-							</div>
-
-							<div className='form-group'>
-								<button className='btn btn-primary btn-block'>Sign Up</button>
-							</div>
-						</div>
-					)}
-
-					{message && (
+			<Form onSubmit={handleRegister} ref={form}>
+				{!successful && (
+					<div>
 						<div className='form-group'>
-							<div
-								className={
-									successful ? 'alert alert-success' : 'alert alert-danger'
-								}
-								role='alert'
-							>
-								{message}
-							</div>
+							<label htmlFor='firstName'>First Name</label>
+							<Input
+								type='text'
+								className='form-control'
+								name='firstName'
+								value={firstName}
+								onChange={onChangeFirstName}
+								validations={[required, vname]}
+							/>
 						</div>
-					)}
-					<CheckButton style={{ display: 'none' }} ref={checkBtn} />
-				</Form>
-			</div>
-		</div>
+
+						<div className='form-group'>
+							<label htmlFor='lastName'>Last Name</label>
+							<Input
+								type='text'
+								className='form-control'
+								name='lastName'
+								value={lastName}
+								onChange={onChangeLastName}
+								validations={[required, vname]}
+							/>
+						</div>
+
+						<div className='form-group'>
+							<label htmlFor='username'>Username</label>
+							<Input
+								type='text'
+								className='form-control'
+								name='username'
+								value={username}
+								onChange={onChangeUsername}
+								validations={[required, vusername]}
+							/>
+						</div>
+
+						<div className='form-group'>
+							<label htmlFor='email'>Email</label>
+							<Input
+								type='text'
+								className='form-control'
+								name='email'
+								value={email}
+								onChange={onChangeEmail}
+								validations={[required, validEmail]}
+							/>
+						</div>
+
+						<div className='form-group'>
+							<label htmlFor='password'>Password</label>
+							<Input
+								type='password'
+								className='form-control'
+								name='password'
+								value={password}
+								onChange={onChangePassword}
+								validations={[required, vpassword]}
+							/>
+						</div>
+
+						<div className='form-group'>
+							<button className='btn btn-primary btn-block btn-mt20'>
+								Sign Up
+							</button>
+						</div>
+					</div>
+				)}
+
+				{message && (
+					<div className='form-group'>
+						<div
+							className={
+								successful ? 'alert alert-success' : 'alert alert-danger'
+							}
+							role='alert'
+						>
+							{message}
+						</div>
+					</div>
+				)}
+				<CheckButton style={{ display: 'none' }} ref={checkBtn} />
+			</Form>
+		</>
 	);
 };
 
